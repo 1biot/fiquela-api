aa5ab06dd6c8645be5dfdbee6bb107866ae7b8f7
Tomáš Lebeda <lebeda@users.noreply.github.com>
2025-05-22 17:51:14 +0200

    Fixed validating schema by pattern

--- src/Schemas/Auth.php
+++ src/Schemas/Auth.php
@@ -14,12 +14,7 @@ class Auth implements Schema
                 'username' => Expect::string()->required(),
                 'password' => Expect::string()->required()
                     ->min(8)
-                    ->pattern('~^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+]).+$~')
-                    ->assert(
-                        fn ($value) => is_string($value),
-                        'Password must contain at least one letter, one uppercase letter, one number'
-                        .' and one special character (!@#$%^&*()_+])'
-                    ),
+                    ->pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+]).+$'),
             ]
         );
     }
